8c9e8df8591242f0aee130ac155e817d2714d296
KukrejaDhriti <83954166+KukrejaDhriti@users.noreply.github.com>
2021-07-17 15:49:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var noseX= 0;
+var noseY= 0;
+
+function preload()
+{
+   clown_nose= loadImage('https://i.postimg.cc/W1Gpgz7H/clown-nose-clipart-2.png');
+} 
+
+function setup()
+{
+    canvas= createCanvas(300, 300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotposes);
+}
+function modelLoaded()
+{
+    console.log("poseNet is initialised");
+}
+function gotposes(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        noseX= results[0].pose.nose.x;
+        console.log("nose x= "+noseX);
+        noseY= results[0].pose.nose.y;
+        console.log("nose y= "+noseY);
+    }
+}
+function draw()
+{
+image(video, 0,0 , 300, 300);
+image(clown_nose, noseX-15, noseY-5, 30, 30);
+}
+
+function take_snapshot()
+{
+    save('myImage.png');
+}
+
